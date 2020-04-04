f21e7945c085ca08ec9a79decc865329c12b2ba6
cher-di <cher-di@mail.ru>
2020-04-04 17:29:53 +0500

    Updated main.py for using new config parser

--- main.py
+++ main.py
@@ -8,26 +8,24 @@ from scripts.config.parse_config import parse_config
 def parse_arguments() -> dict:
     parser = argparse.ArgumentParser(description="Script to start bot")
     parser.add_argument("-c", "--config",
-                        dest="path_to_config",
+                        dest="config_file_path",
                         required=True,
                         help="Path to config file")
     args = parser.parse_args()
 
-    path_to_config = args.path_to_config
-
     return {
-        "path_to_config": path_to_config
+        "config_file_path": args.config_file_path,
     }
 
 
 if __name__ == '__main__':
     args = parse_arguments()
-    config = parse_config(args["path_to_config"])
+    config = parse_config(args["config_file_path"])
     token = config["token"]
 
-    with open(config["path_to_cities"], "rt", encoding="utf8") as file:
+    with open(config["cities_db_file_path"], "rt", encoding="utf8") as file:
         cities_info = csv.DictReader(file)
         _cities = tuple(row["city"] for row in cities_info)
 
-    bot = Bot(token, _cities, config["logs_path"])
+    bot = Bot(token, _cities, config["logs_file_path"])
     bot.run()
